3f40094bffc938f4c48dd3d7a953261645d26a76
Thurston Yates <thurstonalexyates@gmail.com>
2025-02-01 00:26:22 -0500

    Create ParseError, add logic to find columns

--- src/utility/parser.ts
+++ src/utility/parser.ts
@@ -1,6 +1,35 @@
-import { Sheet } from "xlsx";
+import XLSX from "xlsx";
 import { Schedule } from "./schedule";
 
-function parseSheet(sheet: Sheet): Schedule {
+class ParseError extends Error {
+	constructor(message: string) {
+		super(message);
+		this.name = "ParseError";
+		this.message = message;
+	}
+}
+
+function parseSheet(sheet: XLSX.Sheet): Schedule {
+	if (!sheet["!data"]) throw new ParseError("Sheet is empty");
+
+	// Find the row of the column headers for each piece of data,
+	// since variations of the spreadsheet exist where this can be in
+	// different places
+	let headerRow;
+	for (let r = 0; r < sheet["!data"].length; r++) {
+		const row = sheet["!data"][r];
+		if (row[0].v === "My Enrolled Courses") {
+			headerRow = r + 2;
+			break;
+		}
+	}
+
+	if (!headerRow) throw new ParseError("Unable to find the header row");
+
+	// TODO: Find appropriate columns for each piece of information
+	// TODO: For each row of information, create a new section
+	// TODO: Format event name as "{Course ID} {Instructional Format}"
+	// TODO: Extract meeting pattern, start time, end time, and location from "Meeting Patterns" column
+
 	return new Schedule();
 }
