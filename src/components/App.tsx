90468a5c6ff8fe4f8ff38ed3f885c25b68a184ee
Thurston Yates <thurstonalexyates@gmail.com>
2025-03-02 16:49:28 -0500

    Make info section better

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -83,21 +83,19 @@ function App() {
 				</div>
 			</div>
 
-			<div className="info box" hidden={showState !== ShowState.Info}>
+			<div className="box" hidden={showState !== ShowState.Info}>
 				<h3>Instructions</h3>
+				<p>Export your schedule from Workday by navigating to</p>
+				<b>Academics {">"} View My Courses</b>
 				<p>
-					Export your schedule from Workday and upload it here. You can export
-					your schedule from Workday by navigating to{" "}
-					<i>Academics {">"} View My Courses</i> and clicking the Excel icon
-					next to <i>My Enrolled Courses</i>.
+					and clicking the Excel icon above the <b>My Enrolled Courses</b>{" "}
+					table.
 				</p>
 				<p>
-					Note that there are multiple ways of exporting your schedule, and not
-					all formats work (despite my best efforts). The method above should be
-					the most reliable.
+					Upload the <code>.xlsx</code> file here and click download to generate
+					a <code>.ics</code> file that can be imported to any calendar app.
 				</p>
-
-				<h3>Privacy</h3>
+				<h3>Privacy Statement</h3>
 				<p>
 					This tool runs entirely in <i>your</i> browser, and does not store or
 					send any data to a server.
