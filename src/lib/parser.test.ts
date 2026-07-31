1a505523ff233c0f57e590d75708cdd481046fa5
Thurston Yates <thurstonalexyates@gmail.com>
2026-07-31 14:11:58 -0400

    Add top level describe block

--- src/lib/parser.test.ts
+++ src/lib/parser.test.ts
@@ -1,50 +1,52 @@
 import { describe, expect, it } from "vitest";
 import { findColumns, ParseError, parseTimeString } from "../lib/parser";
 
-describe("findColumns", async () => {
-	const sheetData = [
-		["A", "B", "C"],
-		["1", "2", "3"],
-	];
+describe("parser", () => {
+	describe("findColumns", async () => {
+		const sheetData = [
+			["A", "B", "C"],
+			["1", "2", "3"],
+		];
 
-	it("should find columns A, B and C", () => {
-		const columns = findColumns(["A", "B", "C"], 0, sheetData);
-		expect(columns).toEqual({ A: 0, B: 1, C: 2 });
+		it("should find columns A, B and C", () => {
+			const columns = findColumns(["A", "B", "C"], 0, sheetData);
+			expect(columns).toEqual({ A: 0, B: 1, C: 2 });
+		});
+
+		it("should not find nonexistant column D", () => {
+			expect(() => findColumns(["D"], 0, sheetData)).toThrow(ParseError);
+		});
+
+		it("should error if header row is invalid", () => {
+			expect(() => findColumns(["A"], 2, sheetData)).toThrow(
+				"Invalid header row index",
+			);
+		});
 	});
 
-	it("should not find nonexistant column D", () => {
-		expect(() => findColumns(["D"], 0, sheetData)).toThrow(ParseError);
-	});
+	describe("parseTimeString", async () => {
+		it("should parse 12:00 AM", () => {
+			expect(parseTimeString("12:00 AM")).toEqual([0, 0]);
+		});
 
-	it("should error if header row is invalid", () => {
-		expect(() => findColumns(["A"], 2, sheetData)).toThrow(
-			"Invalid header row index"
-		);
-	});
-});
+		it("should parse 12:00 PM", () => {
+			expect(parseTimeString("12:00 PM")).toEqual([12, 0]);
+		});
 
-describe("parseTimeString", async () => {
-	it("should parse 12:00 AM", () => {
-		expect(parseTimeString("12:00 AM")).toEqual([0, 0]);
-	});
+		it("should parse 1:30 PM", () => {
+			expect(parseTimeString("1:30 PM")).toEqual([13, 30]);
+		});
 
-	it("should parse 12:00 PM", () => {
-		expect(parseTimeString("12:00 PM")).toEqual([12, 0]);
-	});
+		it("should parse 1:30 AM", () => {
+			expect(parseTimeString("1:30 AM")).toEqual([1, 30]);
+		});
 
-	it("should parse 1:30 PM", () => {
-		expect(parseTimeString("1:30 PM")).toEqual([13, 30]);
-	});
+		it("should parse 11:59 PM", () => {
+			expect(parseTimeString("11:59 PM")).toEqual([23, 59]);
+		});
 
-	it("should parse 1:30 AM", () => {
-		expect(parseTimeString("1:30 AM")).toEqual([1, 30]);
-	});
-
-	it("should parse 11:59 PM", () => {
-		expect(parseTimeString("11:59 PM")).toEqual([23, 59]);
-	});
-
-	it("should error on invalid time", () => {
-		expect(() => parseTimeString("13:00 PM")).toThrow("Invalid time string");
+		it("should error on invalid time", () => {
+			expect(() => parseTimeString("13:00 PM")).toThrow("Invalid time string");
+		});
 	});
 });
