194b31fa9afd110e0a284184980e17b4cf419db6
Thurston Yates <thurstonalexyates@gmail.com>
2026-07-31 14:05:39 -0400

    Remove daylight savings warning

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -84,16 +84,6 @@ export function IndexPage() {
 					type="warning"
 					message={
 						<p>
-							A previous version of this tool had issues with daylight savings
-							where B-Term classes after the switch would be off by one hour.
-							Check your schedule if you've used this before!
-						</p>
-					}
-				/>
-				<Toast
-					type="warning"
-					message={
-						<p>
 							This tool doesn't currently account for modified schedule days.
 							You'll need to manually adjust your calendar afterward.
 						</p>
